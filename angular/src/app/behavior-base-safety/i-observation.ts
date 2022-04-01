6a31a578a3414286edab10ac97dcbba6e279c288
alexei ougriniouk <000776331@mohawkcollege.ca>
2022-04-01 18:46:43 -0400

    Updated Frontend Stuff

--- angular/src/app/behavior-base-safety/i-observation.ts
+++ angular/src/app/behavior-base-safety/i-observation.ts
@@ -7,10 +7,7 @@ export interface iObservation
 	supervisor ?: string
 	notes ?: string
 	recommendation ?: string
-<<<<<<< HEAD
 	actionRequired ?: string 
-=======
->>>>>>> 50cddb0018e73587d801050aa03ad33cec65b210
 	actionTaken ?: string
 	feedbackToCoworkers ?: string
 	createdBy ?: string
